8e3e2ca06ce1d6c91f7fe3e200b4361aaebb6227
MrCichy94 <przemyslav.cichy@gmail.com>
2020-08-15 18:48:39 +0200

    fix blankpage in safarii ios iphone mobile - another try

--- src/main/java/pl/cichy/onlineshop/controller/CartController.java
+++ src/main/java/pl/cichy/onlineshop/controller/CartController.java
@@ -2,13 +2,19 @@ package pl.cichy.onlineshop.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+import pl.cichy.onlineshop.exception.ProductNotFoundException;
 import pl.cichy.onlineshop.model.Cart;
+import pl.cichy.onlineshop.model.CartItem;
+import pl.cichy.onlineshop.model.Product;
 import pl.cichy.onlineshop.service.CartService;
+import pl.cichy.onlineshop.service.ProductService;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -18,9 +24,11 @@ public class CartController {
 
     private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
     private final CartService cartService;
+    private final ProductService productService;
 
-    public CartController(CartService cartService) {
+    public CartController(CartService cartService, ProductService productService) {
         this.cartService = cartService;
+        this.productService = productService;
     }
 
     //@GetMapping("/{cartId}")
@@ -29,7 +37,7 @@ public class CartController {
         return ResponseEntity.ok(cartService.read(cartId));
     }
 
-    @RequestMapping
+    @GetMapping
     public String get(Model model, HttpServletRequest request) {
 
         //this is prepered empty cart with sesionID vs null cart id exception by thymeleaf
@@ -45,8 +53,81 @@ public class CartController {
     }
 
     //@RequestMapping(value = "/{cartId}", method = RequestMethod.GET)
+
     public String getCart2(@PathVariable(value = "cartId") String cartId, Model model) {
         model.addAttribute("cart", cartService.read(cartId));
         return "cart";
     }
+
+
+    //REMOVING INTEM FROM CART - 2 controllers! Main remove + helping redirecting to solve problem in mobile SAFARI (iphone)
+    @RequestMapping("/{productId}")
+    public String removeItem(@PathVariable String productId, HttpServletRequest request, Model model) {
+        String sessionId = request.getSession(true).getId();
+        Cart cart = cartService.read(sessionId);
+
+        if (cart == null) {
+            cart = cartService.create(new Cart(sessionId));
+        }
+
+        Product product = productService.getProductById(productId);
+        if (product == null) {
+            throw new IllegalArgumentException(new ProductNotFoundException(productId));
+        }
+
+        cart.removeCartItem(new CartItem(product));
+        product.setUnitsInStock(product.getUnitsInStock() + product.getUnitsInOrder());
+        product.setUnitsInOrder(0);
+        logger.info("Item removed from cart");
+        cartService.update(sessionId, cart);
+        model.addAttribute("cart", cartService.read(sessionId));
+        return "redirect:/cart";
+    }
+
+    //this controller is duplicated from /cart cuz we got redirect:/cart (/cart/cart send to /cart)
+    @GetMapping("/cart")
+    public String getFresh(Model model, HttpServletRequest request) {
+
+        //this is prepered empty cart with sesionID vs null cart id exception by thymeleaf
+        String sessionId = request.getSession(true).getId();
+        Cart cart = cartService.read(sessionId);
+        if (cart == null) {
+            cart = cartService.create(new Cart(sessionId));
+        }
+        cartService.update(sessionId, cart);
+
+        model.addAttribute("cart", cartService.read(sessionId));
+        return "cart";
+    }
+
+    //ADDING ITEM TO CART
+    @RequestMapping(value = "/add/{productId}")
+    public String addItem(@PathVariable String productId, HttpServletRequest request, Model model) {
+        String sessionId = request.getSession(true).getId();
+        Cart cart = cartService.read(sessionId);
+        if (cart == null) {
+            cart = cartService.create(new Cart(sessionId));
+        }
+
+        Product product = productService.getProductById(productId);
+        if (product == null) {
+            throw new IllegalArgumentException(new ProductNotFoundException(productId));
+        }
+
+        //all this method i'm gonna move on to servis
+        if (product.getUnitsInStock() > 0) {
+            cart.addCartItem(new CartItem(product));
+            product.setUnitsInStock(product.getUnitsInStock() - 1);
+            product.setUnitsInOrder(product.getUnitsInOrder() + 1);
+            logger.info("Item added to cart");
+            cartService.update(sessionId, cart);
+        } else {
+            logger.info("Item can not be added to cart");
+            //need to fix sending answer to user, there is no more product with given id
+        }
+        return "redirect:/cart";
+
+    }
+
+
 }
