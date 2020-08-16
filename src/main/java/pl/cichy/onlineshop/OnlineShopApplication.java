97c3f116712e27884779a59febdcc56e3dbfcd2f
MrCichy94 <przemyslav.cichy@gmail.com>
2020-08-17 00:16:34 +0200

    trying connect to db postgres heroku

--- src/main/java/pl/cichy/onlineshop/OnlineShopApplication.java
+++ src/main/java/pl/cichy/onlineshop/OnlineShopApplication.java
@@ -6,6 +6,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import javax.validation.Validator;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class OnlineShopApplication {
@@ -17,6 +22,16 @@ public class OnlineShopApplication {
 	@Bean
 	Validator validator() {
 		return new LocalValidatorFactoryBean();
+	}
+
+	private static Connection getConnection() throws URISyntaxException, SQLException {
+		URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+		String username = dbUri.getUserInfo().split(":")[0];
+		String password = dbUri.getUserInfo().split(":")[1];
+		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+
+		return DriverManager.getConnection(dbUrl, username, password);
 	}
 
 }
