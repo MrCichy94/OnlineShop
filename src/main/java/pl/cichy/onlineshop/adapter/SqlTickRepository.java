d697676992418b9950acbce0211bd7107f58088c
MrCichy94 <przemyslav.cichy@gmail.com>
2020-11-03 20:29:29 +0100

    dead code clean

--- src/main/java/pl/cichy/onlineshop/adapter/SqlTickRepository.java
+++ src/main/java/pl/cichy/onlineshop/adapter/SqlTickRepository.java
@@ -2,9 +2,7 @@ package pl.cichy.onlineshop.adapter;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import pl.cichy.onlineshop.model.Comment;
 import pl.cichy.onlineshop.model.Tick;
-import pl.cichy.onlineshop.model.repository.CommentRepository;
 import pl.cichy.onlineshop.model.repository.TickRepository;
 
 @Repository
--- src/main/java/pl/cichy/onlineshop/controller/CartController.java
+++ src/main/java/pl/cichy/onlineshop/controller/CartController.java
@@ -116,6 +116,4 @@ public class CartController {
         return "redirect:/cart";
 
     }
-
-
 }
